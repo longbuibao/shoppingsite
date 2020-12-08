5ffb8cb34dfee92872a14fc1bc5d40bc1aff4012
ban-chanviet <blong1102@gmail.com>
2020-12-08 17:19:38 +0700

    day1

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).on('click', '.search', function() {
+    $('.search-bar').addClass('search-bar-acticve');
+})
+$(document).on('click', '.search-cancle', function() {
+    $('.search-bar').removeClass('search-bar-acticve');
+})
