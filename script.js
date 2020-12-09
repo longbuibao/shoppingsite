e527344f9fee277c02bf02e723e8ff62999dbfa1
ban-chanviet <blong1102@gmail.com>
2020-12-09 19:52:25 +0700

    day 2

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+//search bar
 $(document).on('click', '.search', function() {
-    $('.search-bar').addClass('search-bar-acticve');
+    $('.search-bar').addClass('search-bar-active');
 })
 $(document).on('click', '.search-cancle', function() {
-    $('.search-bar').removeClass('search-bar-acticve');
+        $('.search-bar').removeClass('search-bar-active');
+    })
+    //login form
+$(document).on('click', '.user', function() {
+    $('.form').addClass('user-active');
+})
+$(document).on('click', '.form-cancle', function() {
+        $('.form').removeClass('user-active');
+        $('.form').removeClass('sign-up-active');
+        $('.form').removeClass('forget-active');
+    })
+    //sign-up form
+$(document).on('click', '.sign-up-btn', function() {
+    $('.form').addClass('sign-up-active');
+    $('.form').removeClass('user-active');
+})
+$(document).on('click', '.haveAccount', function() {
+        $('.form').addClass('user-active');
+        $('.form').removeClass('sign-up-active');
+        $('.form').removeClass('forget-active');
+    })
+    //forgot form show up
+$(document).on('click', '.forget', function() {
+    $('.form').addClass('forget-active');
+    $('.form').removeClass('user-active');
 })
