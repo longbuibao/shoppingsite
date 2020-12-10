cd24fd6641a79fb3df853847a232c23175db87f6
ban-chanviet <blong1102@gmail.com>
2020-12-10 16:25:53 +0700

    change directory

--- js/script.js
+++ js/script.js
@@ -29,3 +29,14 @@ $(document).on('click', '.forget', function() {
     $('.form').addClass('forget-active');
     $('.form').removeClass('user-active');
 })
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 3000);
