be464fa8bc8ea471bd33525312dd38ea16886b40
ban-chanviet <blong1102@gmail.com>
2020-12-27 11:19:44 +0700

    long time no see

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ $(document).on('click', '.search-cancle', function() {
     //login form
 $(document).on('click', '.user', function() {
     $('.form').addClass('user-active');
+    $('.form').removeClass('cart-active');
+
 })
 $(document).on('click', '.form-cancle', function() {
         $('.form').removeClass('user-active');
         $('.form').removeClass('sign-up-active');
         $('.form').removeClass('forget-active');
+        $('.form').removeClass('cart-active');
     })
     //sign-up form
 $(document).on('click', '.sign-up-btn', function() {
@@ -50,4 +53,108 @@ $(document).ready(function() {
         loop: true,
         pauseOnHover: true
     });
+});
+
+// popup cart-items
+$(document).on('click', '#clickcart', function() {
+    $('.form').addClass('cart-active');
+});
+//addcart script
+if (!isNaN(parseInt(localStorage.getItem('numberOfProducts')))) {
+    let numberOfProducts = parseInt(localStorage.getItem('numberOfProducts'));
+    $('.num-cart-product').html(numberOfProducts);
+} else {
+    localStorage.setItem('numberOfProducts', '0')
+}
+
+$(document).on('click', '.add-cart', function() {
+    let numberOfProducts = parseInt(localStorage.getItem('numberOfProducts'));
+    numberOfProducts++;
+    $('.num-cart-product').html(numberOfProducts);
+    localStorage.setItem('numberOfProducts', numberOfProducts);
+});
+
+/*------------------------------------quantyti box--------------------*/
+jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+jQuery('.quantity').each(function() {
+    var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+    btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+});
+//----------------------------checkbox for color-----------------------------------
+let srcOfImg;
+let sizeProduct;
+let quantity;
+$(document).on('click', '.whiteBox, .blackBox, .blueBox', function(event) {
+    let i = document.createElement('i');
+    let img = document.createElement('img');
+    i.setAttribute('class', 'fas fa-check-circle');
+    $('.whiteBox, .blackBox, .blueBox').empty();
+    event.target.appendChild(i);
+    document.getElementById('productImg').setAttribute('src', './images/' + event.target.id)
+    srcOfImg = event.target.id;
+});
+$(document).on('click', '.sizeS, .sizeM, .sizeXL', function(event) {
+    let i = document.createElement('i');
+    $('.sizeS, .sizeM, .sizeXL').css('background-color', 'white');
+    event.target.style.backgroundColor = 'red';
+    event.target.appendChild(i);
+    sizeProduct = event.target.id;
+});
+//number of products in cart
+$(document).on('click', '.myButton', function() {
+    quantity = $('#inputNumber').val();
+    console.log(srcOfImg, sizeProduct, quantity);
+    localStorage.setItem('numberOfProducts', parseInt(quantity) + parseInt(localStorage.getItem('numberOfProducts')));
+    $('.num-cart-product').html(parseInt(localStorage.getItem('numberOfProducts')));
+    let id = localStorage.getItem('justClicked');
+    let productObj = JSON.parse(localStorage.getItem(id))[id];
+    console.log(id, productObj)
+    productObj.quantity = quantity;
+    productObj.src = srcOfImg;
+    localStorage.setItem(id, JSON.stringify(productObj));
+
+});
+//--------------------------end---------------------------------------
+//---------------------get money value-------------------------------
+let index;
+let productArray = [{
+    id: 0,
+    price: 150000,
+    src: './images/wuntitled-9-03.jpg',
+    name: 'T-SHIRT'
+}, ];
+
+//click on product
+$(document).on('click', '.product-img', function(event) {
+    console.log(productArray[event.target.id]);
+    localStorage.setItem('justClicked', event.target.id);
+    index = event.target.id;
+    localStorage.setItem(index, JSON.stringify(productArray));
 });
