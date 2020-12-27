2a57af1b262d9d04131b809069efd4ac6a4a9689
ban-chanviet <blong1102@gmail.com>
2020-12-27 18:05:04 +0700

    made menu filter

--- js/script.js
+++ js/script.js
@@ -212,3 +212,29 @@ $(document).on('click', '.product-img', function(event) {
     index = event.target.id;
     localStorage.setItem(index, JSON.stringify(productArray[index]));
 });
+
+
+//filter
+(function($)
+{
+	"use strict";
+	
+	jQuery(window).on('load', function() {
+		preloader();
+				
+		if(jQuery('.gallery-outer .gallery-items').length > 0){
+			jQuery('.gallery-outer .gallery-items').filterizr();
+		}
+		jQuery('#filter-list li').on("click", function(){
+			jQuery('#filter-list li').removeClass('active');
+			jQuery(this).addClass('active');
+		});
+	});
+		
+	function preloader(){
+		jQuery(".preloaderimg").fadeOut();
+		jQuery(".preloader").delay(200).fadeOut("slow").delay(200, function(){
+			jQuery(this).remove();
+		});
+	}
+})(jQuery);	
